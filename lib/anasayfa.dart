1c019c4f6761fac58949d6b21bbebe1eac4cbaef
Mete <mete.3443@gmail.com>
2025-04-06 20:29:10 +0300

    Kullanıcı girişi, haberler, skorlar eklendi.

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,5 +1,10 @@
+import 'package:donemprojesi/ekranlar/ekranlar/gecmisYarislarSayfasi.dart';
 import 'package:flutter/material.dart';
-
+import 'ekranlar/ekranlar/hesabimEkrani.dart';
+import 'ekranlar/girişEkrani.dart';
+import 'ekranlar/haberEkrani.dart';
+import 'ekranlar/ekranlar/briefEkrani.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +16,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
-  void _onItemTapped(int index) {
+  final List<Widget> _widgetOptions = <Widget>[
+    HaberEkrani(), // Haberler (index 0)
+    GecmisYarislarSayfasi(),
+    Briefekrani(),
+
+    Center(child: Text('Hesap')), // Hesap (index 3) - Giriş ekranına yönlendiriliyor
+  ];
+
+  void _onItemTapped(int index) async {
     setState(() {
       _selectedIndex = index;
     });
+
+    if (index == 3) {
+      final user = FirebaseAuth.instance.currentUser;
+
+      // Oturum açık mı kontrol et
+      if (user == null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => GirisEkrani()),
+        ).then((_) {
+          setState(() {
+            _selectedIndex = 0; // Girişten geri gelince "Haber" sekmesine dön
+          });
+        });
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HesabimEkrani()),
+        ).then((_) {
+          setState(() {
+            _selectedIndex = 0; // Profil ekranından çıkınca da ana sayfaya dön
+          });
+        });
+      }
+    }
   }
 
   @override
@@ -23,15 +61,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Center(
           child: Text(
-            "Ana Sayfa",
+            _selectedIndex == 0
+                ? "Haberler"
+                : _selectedIndex == 1
+                ? "Skorlar"
+                : _selectedIndex == 2
+                ? "AI"
+                : "Hesap",
           ),
         ),
-
         elevation: 0,
       ),
-      body: Center(
-
-      ),
+      body: _selectedIndex < _widgetOptions.length - 1
+          ? _widgetOptions[_selectedIndex]
+          : Center(child: Text("Hesap")), // Hesap için geçici placeholder
       bottomNavigationBar: NavigationBar(
         height: 60,
         backgroundColor: Colors.white,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:donemprojesi/anasayfa.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home:  HomePage(),
